74957e9713145f848f096b808efd7a0462ca424b
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-06-19 12:01:38 +0300

    functions

---
+++ App/Functions.fs
@@ -0,0 +1,12 @@
+﻿module Functions
+
+open System
+
+// 5.5.1
+let g (n) = n + 5 // int -> int
+
+// 5.5.2 
+let gg = fun n -> n + 5 // int -> int
+
+//5.5.3
+let h (x, y) = Math.Sqrt(x * x + y * y) // float * float -> float
