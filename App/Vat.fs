cfc92d7d7a9df7ada609a3ca52609e4a99112e23
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-06-21 19:52:50 +0300

    vat

--- App/Vat.fs
+++ App/Vat.fs
@@ -1,9 +1,12 @@
 ﻿module Vat
 
+// 20.3.1
 let vat n x = x * (1.0 + float n / 100.0)
 
+// 20.3.2
 let unvat n y = y / (1.0 + float n / 100.0)
 
+// 20.3.1
 let min f =
     let rec go n =
         if f n = 0 then n else go (n + 1)
