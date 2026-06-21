bc4d291ec29638512829c644a0fba37e3c14865f
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-06-21 15:34:40 +0300

    syntax fix

--- App/Strings.fs
+++ App/Strings.fs
@@ -3,19 +3,19 @@
 // 17.1
 let rec pow = function
     | ("", _) -> ""
-    | (str, count) when count <= 0 -> ""
+    | (str, times) when times <= 0 -> ""
     | (str, 1) -> str
-    | (str, count) -> str + pow(str, count - 1)
+    | (str, times) -> str + pow(str, times - 1)
 
 // 17.2
-let rec isIthChar  = function
+let sisIthChar  = function
     | (_, index, _) when index < 0 -> false
-    | (str, index, char) when (String.length str) - 1 < index -> false
-    | (str, index, char) -> str[index] = char
+    | (str, index, _) when (String.length str) - 1 < index -> false
+    | (str, index, symbol) -> str.[index] = symbol
 
 // 17.3
 let rec occFromIth = function     
      | (_, index, _) when index < 0 -> 0
      | (str, index, _) when String.length str - 1 < index -> 0
-     | (str, index, char) when str[index] = char -> 1 + occFromIth(str,index + 1,char)
-     | (str, index, char) -> occFromIth(str,index + 1,char)
+     | (str, index, symbol) when str.[index] = symbol -> 1 + occFromIth(str,index + 1,symbol)
+     | (str, index, symbol) -> occFromIth(str,index + 1,symbol)
