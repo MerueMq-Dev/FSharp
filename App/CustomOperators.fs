1e7db2f24130989b72e526c12dbde58fff863eb2
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-06-28 04:43:43 +0300

    CustomOperators

---
+++ App/CustomOperators.fs
@@ -0,0 +1,27 @@
+﻿module CustomOperators
+
+// 23.4.1
+let normalize (g, s, c) =
+    let total = (g * 20 + s) * 12 + c
+    let copper = ((total % 12) + 12) % 12
+    let totalSilver = (total - copper) / 12
+    let silver = ((totalSilver % 20) + 20) % 20
+    let gold = (totalSilver - silver) / 20
+    (gold, silver, copper)
+
+let (.+.) (g1, s1, c1) (g2, s2, c2) =
+    normalize (g1 + g2, s1 + s2, c1 + c2)
+
+let (.-.) (g1, s1, c1) (g2, s2, c2) =
+    normalize (g1 - g2, s1 - s2, c1 - c2)
+
+// 23.4.2
+let (.+) (a: float, b: float) (c: float, d: float) = (a + c, b + d)
+
+let (.-) (a: float, b: float) (c: float, d: float) = (a - c, b - d)
+
+let (.*) (a: float, b: float) (c: float, d: float) = (a * c - b * d, b * c + a * d)
+
+let (./) (a: float, b: float) (c: float, d: float) =
+    let denom = c * c + d * d
+    (a, b) .* (c / denom, -d / denom)
--- App/Program.fs
+++ App/Program.fs
@@ -1,9 +1,79 @@
-﻿open Curry
+﻿open CustomOperators
 
+// 23.4.1 (.+.)
+let testMoneyAdd =
+    [
+        ((1, 0, 0), (0, 0, 0), (1, 0, 0))
+        ((0, 0, 11), (0, 0, 1), (0, 1, 0))
+        ((0, 19, 11), (0, 0, 1), (1, 0, 0))
+        ((1, 0, 128), (0, 0, 0), (1, 10, 8))
+        ((32, 23, 5), (0, 0, 7), (33, 4, 0))
+        ((5, 10, 6), (3, 15, 9), (9, 6, 3))
+    ]
+testMoneyAdd
+|> List.iter (fun (x, y, expected) ->
+    let actual = x .+. y
+    printfn "%A .+. %A = %A (expected %A)" x y actual expected)
 
+// 23.4.1 (.-.)
+let testMoneySub =
+    [
+        ((1, 0, 0), (0, 0, 1), (0, 19, 11))
+        ((1, 0, 0), (0, 0, 0), (1, 0, 0))
+        ((0, 1, 0), (0, 0, 1), (0, 0, 11))
+        ((10, 5, 3), (3, 15, 9), (6, 9, 6))
+        ((33, 4, 0), (0, 0, 7), (32, 23, 5))
+        ((1, 0, 0), (1, 0, 0), (0, 0, 0))
+    ]
+testMoneySub
+|> List.iter (fun (x, y, expected) ->
+    let actual = x .-. y
+    printfn "%A .-. %A = %A (expected %A)" x y actual expected)
 
-let add (x, y) = x + y
-let addCurried = curry add
+// 23.4.2 (.+)
+let testComplexAdd =
+    [
+        ((1.0, 2.0), (3.0, 4.0), (4.0, 6.0))
+        ((0.0, 0.0), (1.0, 1.0), (1.0, 1.0))
+        ((-1.0, 3.0), (1.0, -3.0), (0.0, 0.0))
+    ]
+testComplexAdd
+|> List.iter (fun (x, y, expected) ->
+    let actual = x .+ y
+    printfn "%A .+ %A = %A (expected %A)" x y actual expected)
 
-printfn "%d" (addCurried 3 4)      // 7
-printfn "%d" (uncurry addCurried (3, 4))  // 7
+// 23.4.2 (.-)
+let testComplexSub =
+    [
+        ((3.0, 4.0), (1.0, 2.0), (2.0, 2.0))
+        ((1.0, 1.0), (1.0, 1.0), (0.0, 0.0))
+        ((0.0, 0.0), (1.0, -3.0), (-1.0, 3.0))
+    ]
+testComplexSub
+|> List.iter (fun (x, y, expected) ->
+    let actual = x .- y
+    printfn "%A .- %A = %A (expected %A)" x y actual expected)
+
+// 23.4.2 (.*)
+let testComplexMul =
+    [
+        ((1.0, 0.0), (3.0, 4.0), (3.0, 4.0))
+        ((0.0, 1.0), (0.0, 1.0), (-1.0, 0.0))
+        ((2.0, 3.0), (1.0, -1.0), (5.0, 1.0))
+    ]
+testComplexMul
+|> List.iter (fun (x, y, expected) ->
+    let actual = x .* y
+    printfn "%A .* %A = %A (expected %A)" x y actual expected)
+
+// 23.4.2 (./)
+let testComplexDiv =
+    [
+        ((1.0, 0.0), (1.0, 0.0), (1.0, 0.0))
+        ((4.0, 2.0), (2.0, 0.0), (2.0, 1.0))
+        ((-1.0, 0.0), (0.0, 1.0), (0.0, 1.0))
+    ]
+testComplexDiv
+|> List.iter (fun (x, y, expected) ->
+    let actual = x ./ y
+    printfn "%A ./ %A = %A (expected %A)" x y actual expected)
