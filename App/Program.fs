48c19459c081bd2d2449c0087fcf10ab0b6953a9
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-07-02 04:25:01 +0300

    TimeOfDay2

--- App/Program.fs
+++ App/Program.fs
@@ -1,29 +1,29 @@
-﻿open TimeOfDay
+﻿open TimeOfDay2
 
 let testTimeOfDay =
     [
         // PM > AM
-        ({ hours = 1; minutes = 0; f = "PM" }, { hours = 1; minutes = 0; f = "AM" }, true)
-        ({ hours = 1; minutes = 0; f = "AM" }, { hours = 1; minutes = 0; f = "PM" }, false)
+        ({ hours = 1; minutes = 0; f = PM }, { hours = 1; minutes = 0; f = AM }, true)
+        ({ hours = 1; minutes = 0; f = AM }, { hours = 1; minutes = 0; f = PM }, false)
         // часы больше но AM < PM
-        ({ hours = 11; minutes = 0; f = "AM" }, { hours = 1; minutes = 0; f = "PM" }, false)
-        ({ hours = 1; minutes = 0; f = "PM" }, { hours = 11; minutes = 0; f = "AM" }, true)
+        ({ hours = 11; minutes = 0; f = AM }, { hours = 1; minutes = 0; f = PM }, false)
+        ({ hours = 1; minutes = 0; f = PM }, { hours = 11; minutes = 0; f = AM }, true)
         // сравнение часов (одинаковый f)
-        ({ hours = 5; minutes = 0; f = "AM" }, { hours = 3; minutes = 0; f = "AM" }, true)
-        ({ hours = 3; minutes = 0; f = "AM" }, { hours = 5; minutes = 0; f = "AM" }, false)
+        ({ hours = 5; minutes = 0; f = AM }, { hours = 3; minutes = 0; f = AM }, true)
+        ({ hours = 3; minutes = 0; f = AM }, { hours = 5; minutes = 0; f = AM }, false)
         // минуты больше но часы меньше
-        ({ hours = 3; minutes = 59; f = "AM" }, { hours = 5; minutes = 0; f = "AM" }, false)
-        ({ hours = 5; minutes = 0; f = "AM" }, { hours = 3; minutes = 59; f = "AM" }, true)
+        ({ hours = 3; minutes = 59; f = AM }, { hours = 5; minutes = 0; f = AM }, false)
+        ({ hours = 5; minutes = 0; f = AM }, { hours = 3; minutes = 59; f = AM }, true)
         // сравнение минут (одинаковые f и hours)
-        ({ hours = 5; minutes = 30; f = "AM" }, { hours = 5; minutes = 0; f = "AM" }, true)
-        ({ hours = 5; minutes = 0; f = "AM" }, { hours = 5; minutes = 30; f = "AM" }, false)
+        ({ hours = 5; minutes = 30; f = AM }, { hours = 5; minutes = 0; f = AM }, true)
+        ({ hours = 5; minutes = 0; f = AM }, { hours = 5; minutes = 30; f = AM }, false)
         // равные
-        ({ hours = 5; minutes = 30; f = "AM" }, { hours = 5; minutes = 30; f = "AM" }, false)
-        ({ hours = 5; minutes = 30; f = "PM" }, { hours = 5; minutes = 30; f = "PM" }, false)
+        ({ hours = 5; minutes = 30; f = AM }, { hours = 5; minutes = 30; f = AM }, false)
+        ({ hours = 5; minutes = 30; f = PM }, { hours = 5; minutes = 30; f = PM }, false)
     ]
 
 testTimeOfDay
 |> List.iter (fun (x, y, expected) ->
-    let actual = x .>. y
-    printfn "%d:%d %s .>. %d:%d %s = %b (expected %b)"
-        x.hours x.minutes x.f y.hours y.minutes y.f actual expected)
+    let actual = (.>.) x y
+    printfn "%d:%d .>. %d:%d = %b (expected %b)"
+        x.hours x.minutes y.hours y.minutes actual expected)
---
+++ App/TimeOfDay2.fs
@@ -0,0 +1,14 @@
+﻿module TimeOfDay2
+
+type F = 
+  | AM
+  | PM
+
+type TimeOfDay = { hours : int; minutes : int; f: F }
+
+let (.>.) x y =
+        match x, y with
+        | { f = PM }, { f = AM } -> true
+        | { f = AM }, { f = PM } -> false
+        | x, y -> x.hours * 60 + x.minutes > y.hours * 60 + y.minutes
+
