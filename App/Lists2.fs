c7d35245825d340abf95bc8413654f6e2ac5afe1
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-07-04 20:42:54 +0300

    Lists2

---
+++ App/Lists2.fs
@@ -0,0 +1,38 @@
+﻿module Lists2
+
+// 39.1
+let rec rmodd = function
+    | [] -> []
+    | [_] -> []
+    | _ :: x :: xs -> x :: rmodd xs
+
+// 39.2
+let rec del_even = function
+    | [] -> []
+    | x :: xs ->
+        if x % 2 = 0 then
+            del_even xs
+        else
+            x :: del_even xs
+
+// 39.3
+let rec multiplicity x xs =
+    match xs with
+    | [] -> 0
+    | y :: ys ->
+        (if x = y then 1 else 0) + multiplicity x ys
+
+// 39.4
+let rec split = function
+    | [] -> ([], [])
+    | [x] -> ([x], [])
+    | x :: y :: xs ->
+        let (xs1, xs2) = split xs
+        (x :: xs1, y :: xs2)
+
+// 39.5
+let rec zip (xs1, xs2) =
+    match (xs1, xs2) with
+    | ([], []) -> []
+    | (x :: xs, y :: ys) -> (x, y) :: zip (xs, ys)
+    | _ -> failwith "Lists have different lengths"
--- App/Program.fs
+++ App/Program.fs
@@ -1,43 +1,70 @@
-﻿open Lists
+﻿open Lists2
 
-// 34.1
-let testUpto =
+// 39.1
+let testRmodd =
     [
-        (1, [1])
-        (3, [1; 2; 3])
-        (5, [1; 2; 3; 4; 5])
-        (0, [])
+        ([1; 2; 3; 4; 5], [2; 4])
+        ([1; 2; 3], [2])
+        ([1], [])
+        ([], [])
     ]
 
-testUpto
-|> List.iter (fun (n, expected) ->
-    let actual = upto n
-    printfn "upto %d = %A (expected %A)" n actual expected)
+testRmodd
+|> List.iter (fun (xs, expected) ->
+    let actual = rmodd xs
+    printfn "rmodd %A = %A (expected %A)" xs actual expected)
 
-// 34.2
-let testDnto =
+// 39.2
+let testDelEven =
     [
-        (1, [1])
-        (3, [3; 2; 1])
-        (5, [5; 4; 3; 2; 1])
-        (0, [])
+        ([1; 2; 3; 4; 5], [1; 3; 5])
+        ([2; 4; 6], [])
+        ([1; 3; 5], [1; 3; 5])
+        ([], [])
     ]
 
-testDnto
-|> List.iter (fun (n, expected) ->
-    let actual = dnto n
-    printfn "dnto %d = %A (expected %A)" n actual expected)
+testDelEven
+|> List.iter (fun (xs, expected) ->
+    let actual = del_even xs
+    printfn "del_even %A = %A (expected %A)" xs actual expected)
 
-// 34.3
-let testEvenn =
+// 39.3
+let testMultiplicity =
     [
-        (1, [0])
-        (3, [0; 2; 4])
-        (5, [0; 2; 4; 6; 8])
-        (0, [])
+        (1, [1; 2; 1; 3; 1], 3)
+        (2, [1; 2; 3], 1)
+        (4, [1; 2; 3], 0)
+        (1, [], 0)
     ]
 
-testEvenn
-|> List.iter (fun (n, expected) ->
-    let actual = evenn n
-    printfn "evenn %d = %A (expected %A)" n actual expected)
+testMultiplicity
+|> List.iter (fun (x, xs, expected) ->
+    let actual = multiplicity x xs
+    printfn "multiplicity %d %A = %d (expected %d)" x xs actual expected)
+
+// 39.4
+let testSplit =
+    [
+        ([1; 2; 3; 4], ([1; 3], [2; 4]))
+        ([1; 2; 3], ([1; 3], [2]))
+        ([1], ([1], []))
+        ([], ([], []))
+    ]
+
+testSplit
+|> List.iter (fun (xs, expected) ->
+    let actual = split xs
+    printfn "split %A = %A (expected %A)" xs actual expected)
+
+// 39.5
+let testZip =
+    [
+        (([1; 2; 3], [4; 5; 6]), [(1,4); (2,5); (3,6)])
+        (([1], [2]), [(1,2)])
+        (([], []), [])
+    ]
+
+testZip
+|> List.iter (fun (xs, expected) ->
+    let actual = zip xs
+    printfn "zip %A = %A (expected %A)" xs actual expected)
