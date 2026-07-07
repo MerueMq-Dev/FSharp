129d5d6aaacc69a347fbf0c944213f03d2996e33
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-07-07 20:41:00 +0300

    set-helpers

--- App/Program.fs
+++ App/Program.fs
@@ -1,44 +1,16 @@
-﻿open Lists3
+﻿open SetHelpers
 
-// 41.4.1
-let testListFilter =
+let testAllSubsets =
     [
-        ((fun x -> x % 2 = 0), [1; 2; 3; 4; 5], [2; 4])
-        ((fun x -> x > 3), [1; 2; 3; 4; 5], [4; 5])
-        ((fun x -> true), [1; 2; 3], [1; 2; 3])
-        ((fun x -> false), [1; 2; 3], [])
-        ((fun x -> x > 0), [], [])
+        (3, 0, [[]])
+        (3, 1, [[1]; [2]; [3]])
+        (3, 2, [[1; 2]; [1; 3]; [2; 3]])
+        (3, 3, [[1; 2; 3]])
+        (4, 2, [[1; 2]; [1; 3]; [1; 4]; [2; 3]; [2; 4]; [3; 4]])
+        (0, 0, [[]])
     ]
 
-testListFilter
-|> List.iter (fun (f, xs, expected) ->
-    let actual = list_filter f xs
-    printfn "list_filter f %A = %A (expected %A)" xs actual expected)
-
-// 41.4.2
-let testSum =
-    [
-        ((fun x -> x % 2 = 0), [1; 2; 3; 4; 5], 6)
-        ((fun x -> x > 3), [1; 2; 3; 4; 5], 9)
-        ((fun x -> false), [1; 2; 3], 0)
-        ((fun x -> x > 0), [], 0)
-    ]
-
-testSum
-|> List.iter (fun (p, xs, expected) ->
-    let actual = sum(p, xs)
-    printfn "sum p %A = %d (expected %d)" xs actual expected)
-
-// 41.4.3
-let testRevrev =
-    [
-        ([[1; 2]; [3; 4; 5]], [[5; 4; 3]; [2; 1]])
-        ([[1]; [2]; [3]], [[3]; [2]; [1]])
-        ([[1; 2; 3]], [[3; 2; 1]])
-        ([], [])
-    ]
-
-testRevrev
-|> List.iter (fun (xs, expected) ->
-    let actual = revrev xs
-    printfn "revrev %A = %A (expected %A)" xs actual expected)
+testAllSubsets
+|> List.iter (fun (n, k, expected) ->
+    let actual = allSubsets n k
+    printfn "allSubsets %d %d = %A (expected %A)" n k actual expected)
---
+++ App/SetHelpers.fs
@@ -0,0 +1,10 @@
+﻿module SetHelpers
+
+// 42.3
+let rec allSubsets n k =
+    if k = 0 then set [ Set.empty ]
+    elif n < k then set []
+    else
+        Set.union
+            (allSubsets (n - 1) k)
+            (Set.map (Set.add n) (allSubsets (n - 1) (k - 1)))
